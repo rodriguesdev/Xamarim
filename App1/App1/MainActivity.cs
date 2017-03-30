ffa6143b3b02a02d5e3170e18bcb112c92307e8f
Leandro Rodrigues <leandro.lacr@yahoo.com.br>
2017-03-29 21:23:44 -0300

    Alterações iniciais em MainActivity.cs

--- App1/App1/MainActivity.cs
+++ App1/App1/MainActivity.cs
@@ -1,18 +1,26 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System;
 
 namespace App1
 {
     [Activity(Label = "App1", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        protected override void OnCreate(Bundle bundle)
+        int count = 1;
+
+        protected override void OnCreate(Bundle SavedInstanceState)
         {
-            base.OnCreate(bundle);
+            base.OnCreate(SavedInstanceState);
 
             // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
             // SetContentView (Resource.Layout.Main);
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count); };
         }
     }
 }
